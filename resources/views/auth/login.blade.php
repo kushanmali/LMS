731c2ddd2823a1a250051d4c4e0640bd60238b15
kushanmali <kushanmalinda200@gmail.com>
2024-03-14 15:39:14 +0530

    login commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,53 +1,89 @@
 
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign In</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="{{asset('/asset/fonts/material-icon/css/material-design-iconic-font.min.css')}}">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="{{asset('/asset/css/style.css')}}">
+</head>
+<body>
+
+    <div class="main">
+
+        <section class="sign-in">
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="{{asset('/asset/images/signin-image.jpg')}}" alt="sing up image"></figure>
+                        <a href="{{ route('register') }}" class="signup-image-link">Create an account</a>
+                    </div>
+        
+                    <div class="signin-form">
+                        <h2 class="form-title">Sign In</h2>
+
+                        <form method="POST" action="{{ route('login') }}" class="register-form" id="login-form">
+                            @csrf
+
+
+                            <div class="form-group">
+
+                                <label for="email"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Rmail"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="password" placeholder="Password"/>
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" name="remember_me" id="remember_me" class="agree-term" />
+                                <label for="remember_me" class="label-agree-term"><span><span></span></span>Remember me</label>
+                            </div>
+
+                            <div class="form-group form-button">
+                             @if (Route::has('password.request'))
+                                <input type="submit" name="signin" id="signin" class="submitform-" value="Log in"
+                                />
+                               
+                                
+
+                                <a href="{{ route('password.request') }}" class="forgot-password-link">Forgot Password?</a>
+
+                             @endif
+                            </div>
+
+
+
+                          
+                            
+                        </form>
+                       
+                    </div>
+                </div>
+            </div>
+        </section>
+
+      
+
+        
+        
+
+    </div>
+
+    <!-- JS -->
+    <script src="{{asset('/asset/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('/asset/js/main.js')}}"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
+
 
 
  
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> 
