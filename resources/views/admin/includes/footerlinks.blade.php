95748d3537913d811eefefbf157f7ca09cdfcb42
kushanmali <kushanmalinda200@gmail.com>
2024-03-14 15:34:23 +0530

    register commit

--- resources/views/admin/includes/footerlinks.blade.php
+++ resources/views/admin/includes/footerlinks.blade.php
@@ -4,3 +4,6 @@
 <script src="{{asset('/assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
 <!-- main script file  -->
 <script src="{{asset('/assets/js/soft-ui-dashboard-pro-tailwind.js?v=1.0.1')}}"></script>
+
+
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,149 @@
-<x-guest-layout>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="{{asset('/asset/fonts/material-icon/css/material-design-iconic-font.min.css')}}">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="{{asset('/asset/css/style.css')}}">
+</head>
+<body>
+    
+
+  
+
+    <div class="main">
+
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">Sign up</h2>
+                        
+                        <form method="POST" action="{{ route('register') }}" class="register-form" id="register-form">
+                            @csrf
+
+                            @if($errors->any())
+                            <div class="bg-red-300 text-white rounded-3 p-6 my-5">
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                            </div>
+                              @endif
+
+                              <div class="form-group">
+                                <!-- Name -->
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="name" id="name" placeholder="Your Name"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- Email -->
+                                <label for="email"><i class="zmdi zmdi-email"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- Phone Number -->
+                                <label for="phone_number"><i class="zmdi zmdi-phone"></i></label>
+                                <input type="text" name="phone_number" id="phone_number" placeholder="Your Phone Number"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- Password -->
+                                <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="password" placeholder="Password"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- Repeat Password -->
+                                <label for="password_confirmation"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- Birthday -->
+                                <label for="birthday"><i class="zmdi zmdi-calendar"></i></label>
+                                <input type="date" name="birthday" id="birthday" placeholder="Your Birthday"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- Address -->
+                                <label for="address"><i class="zmdi zmdi-pin"></i></label>
+                                <input type="text" name="address" id="address" placeholder="Your Address"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- National ID Number -->
+                                <label for="national_id_number"><i class="zmdi zmdi-card"></i></label>
+                                <input type="text" name="national_id_number" id="national_id_number" placeholder="National ID Number"/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- Year -->
+                                <label for="al_year"><i class="zmdi zmdi-calendar-check"></i></label>
+                                <input type="number" name="al_year" id="al_year" placeholder="AL year"/>
+                            </div>
+
+                        
+                            
+                            
+                            {{-- <div class="form-group">
+                                <!-- Agree to Terms -->
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in <a href="#" class="term-service">Terms of service</a></label>
+                            </div> --}}
+
+                            <div class="form-group form-button">
+                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                                <a class= href = "{{ route('login') }}">
+                                   
+                                </a>
+
+                             
+                                
+                            </div>
+                            
+                            
+                        </form>
+
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="{{asset('/asset/images/signup-image.jpg')}}" alt="sing up image"></figure>
+                        <a href="{{ route('login') }}" class="signup-image-link">I am already member</a>
+                    </div>
+                   
+                </div>
+            </div>
+        </section>
+
+        
+        
+
+    </div>
+
+
+    
+
+    <!-- JS -->
+    <script src="{{asset('/asset/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('/asset/js/main.js')}}"></script>
+</body>
+</html>
+
+
+
+
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -22,7 +167,7 @@
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for=" " :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
@@ -42,7 +187,7 @@
         </div>
 
         <!-- Student Fields -->
-        {{-- <div class="mt-4">
+        <div class="mt-4">
             <x-input-label for="first_name" :value="__('First Name')" />
             <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autocomplete="given-name" />
             <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
@@ -52,7 +197,7 @@
             <x-input-label for="last_name" :value="__('Last Name')" />
             <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autocomplete="family-name" />
             <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
-        </div> --}}
+        </div>
 
         <div class="mt-4">
             <x-input-label for="birthday" :value="__('Birthday')" />
@@ -95,4 +240,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout>  --}}
